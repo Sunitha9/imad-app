0eab37e23c35ff44eed9d718da1c17d3e6c67fb9
Sunitha9 <30764281+Sunitha9@users.noreply.github.com>
2017-08-07 17:19:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,14 +74,21 @@ function createTemplate(data){
    return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 app.get('/:articleName',function(req,res){
     //articlename==article-one
     //articles[articlename]=={}content object for articleone
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
+});
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.tostring());
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
