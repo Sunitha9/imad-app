bbdd3e34d3710734da4fc4a2c0a5aab6a30ec27c
Sunitha9 <30764281+Sunitha9@users.noreply.github.com>
2017-08-07 18:33:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
-console.log('Loaded!');
-//change the text of the main-text div
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
-
-//move an img
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-var interval=setInterval(moveRight,50);
+//counter code
+var button=document.getElementById('counter');
+button.onclick=function(){
+    //create a request object
+    var request=new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadyStatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            //take action
+            if(request.state==200){
+                var counter=request.responseText;
+                var span=document.getElementById('counter');
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+        //not yet done
+    };
+    //make the request
+    request.open('GET',"http://u900ssunitha.imad.hasura-app.io/",true);
+    request.send(null);
 };
