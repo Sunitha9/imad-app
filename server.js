d0ad73e255ac35da238111acce74468d3ff1c649
Sunitha9 <30764281+Sunitha9@users.noreply.github.com>
2017-08-13 20:51:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-app.get('/submit-name/:name',fuction(req,req)){
+app.get('/submit-name/:name',fuction(req,res)){
     //get the name from the request
     var name=req.params.name;
     
