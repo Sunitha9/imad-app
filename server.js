303dc114da4cb0bb1d8712bde157cc397ec3769f
Sunitha9 <30764281+Sunitha9@users.noreply.github.com>
2017-08-15 16:16:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/test_db',function(req,res){
             
         }
         else{
-            res.send(JSON.stringify(result.rows));
+            res.send(JSON.stringify(result));
         }
     });
 });
