26289f7ab31b6559003c149bd56c661ba2363a67
Sunitha9 <30764281+Sunitha9@users.noreply.github.com>
2017-08-15 17:21:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/articles/:articleName',function(req,res){
     //articlename==article-one
     //articles[articlename]=={}content object for articleone
     
-    pool query("SELECT * FROM article WHERE title='" +req.params.articleName+ "'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title='" +req.params.articleName+ "'",function(err,result){
      if(err){
          res.status(500).send(err.toString());
      }  else{
@@ -146,7 +146,7 @@ app.get('/articles/:articleName',function(req,res){
     });
     
    
-})
+});
 
 
 app.get('/ui/main.js', function (req, res) {
